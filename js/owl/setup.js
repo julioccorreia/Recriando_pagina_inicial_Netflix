bb8cbe7e6b73a5e11fb92fc38727779afe0a335a
julioccorreia <juliocgazige@hotmail.com>
2021-03-07 04:26:53 -0300

    Recriando Netflix - first commit

---
+++ js/owl/setup.js
@@ -0,0 +1,19 @@
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 15,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 3,
+        },
+        1000: {
+            items: 5,
+        },
+        1500: {
+            items: 7,
+        },
+    },
+});
